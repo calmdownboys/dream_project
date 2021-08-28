8ddda0869f4450150bc24a9c7d6ec6cba946bc41
Nikita <nektaylor@mail.ru>
2021-08-29 00:47:38 +0500

    view from different file on naked node

--- server.js
+++ server.js
@@ -5,38 +5,42 @@ const port = 3000
 const app = express()
 const fs = require ('fs')
 const server = http.createServer((request, response) => {
-  response.statusCode = 200
-  response.setHeader('Content-Type', 'text/html; charset=utf-8;')
+  
+
   console.log(`Запрошенный адрес: ${request.url}`) // вывод в консоль запрашиваемого адреса
+  fs.readFile('main.html', 'utf8', function(error, data){
 
   if (request.url === '/')
   {
     response.statusCode = 301 //реализован редирект на другую страницу /home 
-    response. setHeader ('Location', '/home')
+    response.setHeader ('Location', '/home')
   }
   else if (request.url === '/home')
   {
     //поптыка отправить ответ в виде информации из другого файла 
-    fs.readFile('main.html', 'utf8', function(error, data){
-                 
       let message = "Это ваша домашняя страница" 
       let header = "Главная страница"
-      data = data.replace('{header}', header).replace('{message}', message);
-      response.end(data);
-  })
-}
+      data = data.replace('{header}', header).replace('{message}', message)
+      
+      response.write(data)
+    
+  }
 
   else if (request.url === '/auth')
   {
-    response.write ('Auth')
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html; charset=utf-8;')
+    response.write('Auth')
   }
   else 
   {
     //если введенный url отличный от задуманных, то отправляем 404
     response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html; charset=utf-8;')
     response.write ('error url, check your address')
   }
-response.end()
+response.end ()
+  })
 })
 
 
